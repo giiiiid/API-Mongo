e1abd6533ae1c5fa4eb1b902b1110aa5b9166e1e
Giiid <gideonfummey1@gmail.com>
2024-04-20 14:42:13 +0100

    first endpoint

--- routes/blog.py
+++ routes/blog.py
@@ -1,5 +1,12 @@
 from fastapi import APIRouter
+from models.models import BlogModel
 
 
 blog = APIRouter()
 
+
+# endpoints
+@blog.post("/blog/create", response_model=BlogModel)
+async def create_blog(blog:BlogModel):
+    new_blog = blog.model_dump()
+    return new_blog
